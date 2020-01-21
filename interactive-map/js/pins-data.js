e44d5d8901774a411462844b00018ffffc7feec6
Shafqat Ali <shafqat@kco.ie>
2020-01-21 23:21:07 +0500

    initial commit.

---
+++ interactive-map/js/pins-data.js
@@ -0,0 +1,82 @@
+var locationsList = {
+    "MapImage": "assets/images/maketa.jpg",
+    "Markers": [
+        {
+            "PosX": "500",
+            "PosY": "600",
+            "HoverInfo": "ktirio 1",
+            "Title": "ktirio 1]",
+            "FolderName": {
+                "@length": "143",
+                "#text": "assets/politikoNosokomeio/"
+            },
+            "Info": "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. \n\nNam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. \n\nMauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim."
+        },
+        {
+            "PosX": "100",
+            "PosY": "900",
+            "HoverInfo": "ktirio 1.5",
+            "Title": "ktirio 1.5]",
+            "FolderName": {
+                "@length": "143",
+                "#text": "assets/politikoNosokomeio/"
+            },
+            "Info": "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit."
+        },
+        {
+            "PosX": "650",
+            "PosY": "600",
+            "HoverInfo": "ktirio 2",
+            "Title": "ktirio 2",
+            "FolderName": {
+                "@length": "143",
+                "#text": "assets/politikoNosokomeio/"
+            },
+            "Info": "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. \n\nNam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. \n\nMauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim."
+        },
+        {
+            "PosX": "550",
+            "PosY": "620",
+            "HoverInfo": "ktirio 2 kai miso",
+            "Title": "ktirio 2 kai miso",
+            "FolderName": {
+                "@length": "143",
+                "#text": "assets/politikoNosokomeio/"
+            },
+            "Info": "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit."
+        },
+        {
+            "PosX": "100",
+            "PosY": "200",
+            "HoverInfo": "ktirio 3",
+            "Title": "ktirio 3",
+            "FolderName": {
+                "@length": "143",
+                "#text": "assets/politikoNosokomeio/"
+            },
+            "Info": "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. \n\nNam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. \n\nMauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim."
+        },
+        {
+            "PosX": "220",
+            "PosY": "200",
+            "HoverInfo": "Αγαλμα του Κολοκοτρώνη Αγαλμα του Κολοκοτρώνη",
+            "Title": "Αγαλμα Κολοκοτρώνη",
+            "FolderName": {
+                "@length": "143",
+                "#text": "assets/politikoNosokomeio/"
+            },
+            "Info": "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. \n\nNam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. \n\nMauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim."
+        },
+        {
+            "PosX": "280",
+            "PosY": "100",
+            "HoverInfo": "5",
+            "Title": "Αγαλμα Κολοκοτρώνη 5",
+            "FolderName": {
+                "@length": "143",
+                "#text": "assets/politikoNosokomeio/"
+            },
+            "Info": "5 This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. \n\nNam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. \n\nMauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim."
+        }
+    ]
+};
---
+++ interactive-map/js/plugin.js
@@ -0,0 +1,99 @@
+var pins = null;
+function bind_maps() {
+    pins = locationsList.Markers;
+    var count = 0;
+    $('.pin-details').hide().removeClass('d-none');
+    $('.annotated-wrapper').each(function () {
+        count += 1;
+        var wrapperId = $(this).attr("id");
+        var filter = false;
+        drawPins(wrapperId, count, filter);
+    });
+
+    //bind tooltips
+    $('.annotated-bg [data-toggle="tooltip"]').tooltip();
+    $('body').tooltip({
+        selector: '[data-toggle=tooltip]'
+    });
+    
+    //bind click event for map pins
+    $('.map-pins span').click(function () {
+        var wrapperId = $(this).attr("data-parent");
+        $('#' + wrapperId + ' .pin-details').hide();
+        var dHref = $(this).attr('data-href');
+        if (dHref.length > 0 && dHref != '#') {
+            fetchPlaceData(wrapperId, dHref.replace('#', ''));
+        }
+    });
+    //scroll to hashed pin
+    if (window.location.hash.length > 0) {
+        $('.pin-details').hide();
+        var hash = window.location.hash.replace('#', '');
+        hash = hash.replace('/', '');
+        if (hash.length) {
+            var wrapperId = $('.annotated-wrapper span[data-href="#' + hash + '"]').attr("data-parent");
+            if(wrapperId !== undefined){
+                fetchPlaceData(wrapperId, hash);
+            }
+        }
+    }
+}
+
+function drawPins(wrapperId, counter, filter) {
+    //console.log('drawPins: wrapperId: '+ wrapperId + ' >>counter: '+ counter +' >>filter: '+ filter);
+    var html = "";
+    for (var i = 0; i < pins.length; i++) {
+        counter = i;
+        var xPos = pins[i]['PosX'];
+        var yPos = pins[i]['PosY'];
+        var hoverInfo = pins[i]['HoverInfo'];
+        var title = pins[i]['Title'];
+        var info = pins[i]['Info'];
+        var folderName = pins[i]['FolderName']['#text'];
+        var pinIdx = 'pin_'+xPos+'_'+yPos;
+        var pinId = slugifyString(title);
+
+        html += '<span style="left: ' + xPos + 'px; top:' + yPos + 'px;" class="marker" id="' + pinId + '-' + counter + '" tabindex="0" data-href="#' + pinId + '" data-toggle="tooltip" data-placement="bottom" title="' + title + '" role="tooltip" aria-label="' + title + '" data-parent="' + wrapperId + '"></span>';
+    }
+    $('#' + wrapperId + ' .annotated-bg .map-pins').append(html);
+}
+
+function fetchPlaceData(wrapperId, dPlace) {
+    //console.log('fetchPlaceData wrapperId: '+ wrapperId + ' >>dPlace,: '+ dPlace);
+    if (dPlace == undefined) {
+        return false;
+    }
+    //get required place form JSON Object
+    var place = pins.filter(
+        function (data) {
+            return slugifyString(data.Title) == dPlace;
+        });
+
+    var placeImage = '<img src="images/temple-zeus.jpg" alt="placeholder" />';
+    $('#' + wrapperId + ' .image').html(placeImage);
+
+    $('#' + wrapperId + ' .name').html(place[0].Title);
+    $('#' + wrapperId + ' .description').html(place[0].Info);
+    $('#' + wrapperId + ' .pin-details').show();
+    scrolledToDetails(wrapperId);
+}
+
+function scrolledToDetails(wrapperId) {
+    setTimeout(function () {
+        $('html, body').stop().animate({scrollTop: $('#' + wrapperId + ' .pin-details').offset().top}, 500);
+    }, 500);
+}
+
+/*
+ This function takes a string and then remove white space,
+ special characters
+ */
+function slugifyString(str) {
+    let trimmed = str.trim();
+    let slug = trimmed.replace(/[^a-z0-9-]/gi, '-').replace(/-+/g, '-').replace(/^-|-$/g, '');
+    var parsed = slug.toLowerCase();
+    if(parsed.length == 0){
+        parsed = trimmed;
+    }
+    return parsed;
+}
